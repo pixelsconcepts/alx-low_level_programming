b481ad41196ef7a5fbb783e63142cbe6c2a65675
pixelsconcepts <realjones2012@gmail.com>
2023-06-16 21:11:00 -0700

    Added files

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,24 +4,30 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar('0' + i);
-		if (i < 9)
+	
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+			putchar('0' + i);
+			putchar('0' + j);
 
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}	
+
+	
+	}
 	putchar('\n');
 
 	return (0);
-
 
 }

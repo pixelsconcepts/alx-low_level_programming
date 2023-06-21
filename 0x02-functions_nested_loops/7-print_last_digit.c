903812d61e8598b720c404e939519c994807b117
pixelsconcepts <realjones2012@gmail.com>
2023-06-21 12:58:50 -0700

    added 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print the last digit of the number
+ * @r: character to be checked
+ * Return: Return last digit
+ */
+
+int print_last_digit(int r)
+{
+int last_digit = r % 10;
+_putchar(last_digit);
+return (0);
+}

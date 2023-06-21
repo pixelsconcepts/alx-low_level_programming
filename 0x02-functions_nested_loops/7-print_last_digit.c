46803ba9f00d1f26c1fa7cb943748e09185e22cc
pixelsconcepts <realjones2012@gmail.com>
2023-06-21 13:37:08 -0700

    updated 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,20 @@
 /**
  * print_last_digit - Print the last digit of the number
  * @r: character to be checked
- * Return: Return last digit
+ * Return: last digit of number r
  */
 
 int print_last_digit(int r)
 {
-int last_digit = r % 10;
-_putchar(last_digit);
-return (0);
+int last_digit;
+if (r < 0)
+{
+last_digit = -1 * (r % 10);
+}
+else
+{
+last_digit = r % 10;
+}
+_putchar((last_digit % 10) + '0');
+return (last_digit % 10);
 }

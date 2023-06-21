dfa9dc9740aad2a5139e64700c7dc7ad39b66582
pixelsconcepts <realjones2012@gmail.com>
2023-06-21 09:27:47 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_abs - check the absolute number of an integer
+ *@r : character tp be checked
+ * return: r is positive . -r otherwise
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+
+}

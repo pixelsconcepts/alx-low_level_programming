fc847f81017225d538920b481fb9a76c8a661384
pixelsconcepts <realjones2012@gmail.com>
2023-06-21 18:20:02 -0700

    updated 104-fibonnaci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
 /**
- * print_fib - prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * @len: the length of with the fib sequance should print to.
- *
- * Return: void
- */
-void print_fib(int len)
-{
-unsigned int target, i;
+*print_fibonacci - Prints fibonacci
+*Return: void
+*/
 
-unsigned int former_l = 0, former_r = 0;
-unsigned int latter_l = 0 , latter_r = 0;
-unsigned int fib_l = 0, fib_r = 0;
-
-f = 1;
-l = 2;
-target = len - 2;
-for (i = 0 ; i < target ; i++)
+void print_fibonacci(void)
 {
-fib = f + l;
-printf("%d : ", i + 4);
-printf("%.0Lf", fib);
-f = l;
-l = fib;
-if (i < target - 1)
+unsigned int num1 = 1;
+unsigned int num2 = 2;
+unsigned int fib;
+int i;
+
+printf("%u, %u, ", num1, num2);
+
+for (i = 3; i <= 98; i++)
+{
+fib = num1 + num2;
+printf("%u", fib);
+
+if (i != 98)
+{
+printf(", ");
+}
+
+num1 = num2;
+num2 = fib;
+}
+
 printf("\n");
 }
-putchar('\n');
-}
-
 
 /**
- * main - Entry point,
- *
- *
- * Return: Always 0 (sucess)
+ * main - Entry point
+ * Return: 0
  */
 int main(void)
 {
-print_fib(98);
+print_fibonacci();
 return (0);
 }
-
-

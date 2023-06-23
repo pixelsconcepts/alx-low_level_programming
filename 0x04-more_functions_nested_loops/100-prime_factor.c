7693b85fd0233e3c7e3eb1bfa824b4b50941a9ac
pixelsconcepts <realjones2012@gmail.com>
2023-06-23 16:04:41 -0700

    added 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point, print prime factor numbers
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+unsigned long int num = 612852475143;
+unsigned long int prim;
+
+prim = 3;
+while (prim < num / 2)
+{
+if ((num % prim) == 0)
+{
+if ((prim % 3) == 2)
+printf(",%lu ", prim);
+}
+prim += 2;
+}
+
+putchar('\n');
+return (0);
+}

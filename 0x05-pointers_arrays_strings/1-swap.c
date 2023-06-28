51c796744b37c95714b5913c9616d6212732046a
pixelsconcepts <realjones2012@gmail.com>
2023-06-28 00:26:23 -0700

    added file 1-main.c and 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of to int parameters a and b
+ * @a: first parament 
+ * @b: second parament
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp_a;
+
+	temp_a = *a;
+
+
+	*a = *b;
+	*b = temp_a;
+}

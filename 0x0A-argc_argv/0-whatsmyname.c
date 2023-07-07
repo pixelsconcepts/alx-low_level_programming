bb58f0f0d6e9489e504e880e1bf9880ecbd6174a
pixelsconcepts <realjones2012@gmail.com>
2023-07-07 23:30:09 +0000

    Added 0-whatsmyname.c, mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of a progran
+ * @argc: Argument integer.
+ * @argv: Argument vector
+ * Return: name of the program
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}

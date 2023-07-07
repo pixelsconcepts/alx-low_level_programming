dd5ff424e7142b545ee564721e69fc40c9b562cc
pixelsconcepts <realjones2012@gmail.com>
2023-07-07 04:50:38 +0000

    added 101-wildcmp.c and 101-main.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - fumction that compares tow strngs
+ * @s1: first string
+ * @s2: second string
+ * return: the compare string
+ */
+
+
+int wildcmp(char *s1, char *s2)
+{
+
+}

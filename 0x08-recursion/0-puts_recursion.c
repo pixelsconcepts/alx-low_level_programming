cf78d83f625e23be04bb7ac7dcb7f24834282e7b
pixelsconcepts <realjones2012@gmail.com>
2023-07-07 03:19:48 +0000

    Added files _putchar.c, 0-puts_recursion.c, 0-main.c, main.h, 0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: Pointer to a variable
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}

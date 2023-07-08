830e2e9cedf22d5da3247ea6fbcbe8bfa9e0a568
pixelsconcepts <realjones2012@gmail.com>
2023-07-08 00:31:47 +0000

    Added 3-mul.c and mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print the name of a progran
+ * @argc: Argument integer.
+ * @argv: Argument vector
+ * Return: name of the program
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int product = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}

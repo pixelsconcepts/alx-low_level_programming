537f515e50af0a45ec26b3433a18b54b743e4b05
pixelsconcepts <realjones2012@gmail.com>
2023-07-13 08:38:51 +0000

    updated main.c and main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,84 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*Prototype from 0x02-functions_nested_loops*/
-
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n);
-int _abs(int r);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-/*Prototype from 0x03-debugging*/
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-
-
-/*Prototype from 0x04-more_functions_nested_loops */
-
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
-/*Prototype from 0x05-pointers_arrays_strings */
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-
-/*Prototype from 0x06-pointers_arrays_strings */
-
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-/*Prototype from 0x07-pointers_arrays_strings */
-
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
 
-/*Prototype from 0x08recursion */
-
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
 #endif

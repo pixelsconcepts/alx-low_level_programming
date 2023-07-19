e56690f17cd3ae0f4beb7742920a2ded3182e031
pixelsconcepts <realjones2012@gmail.com>
2023-07-19 21:59:12 +0000

    Added files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using the specified function
+ * @name: name of the person
+ * @f: function pointer to the print function
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+

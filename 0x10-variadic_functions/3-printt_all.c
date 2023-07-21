6ba324f1a07c1cd0dea71819e25c2c3216ba544c
pixelsconcepts <realjones2012@gmail.com>
2023-07-21 10:51:21 +0000

    fixed 3-print_all.c

--- 0x10-variadic_functions/3-printt_all.c
+++ 0x10-variadic_functions/3-printt_all.c
@@ -13,8 +13,10 @@
 void print_all(const char * const format, ...)
 {
 	va_list pa_args;
-	int i = 0, num;
-	char *str, c;
+	int i = 0;
+	char *str;
+	char c;
+	int num;
 	float f;
 
 	va_start(pa_args, format);
@@ -27,28 +29,38 @@ void print_all(const char * const format, ...)
 				c = va_arg(pa_args, int);
 				printf("%c", c);
 				break;
+
 			case 'i':
 				num = va_arg(pa_args, int);
 				printf("%d", num);
 				break;
+
 			case 'f':
 				f = va_arg(pa_args, double);
 				printf("%f", f);
 				break;
+
 			case 's':
 				str = va_arg(pa_args, char*);
 				if (str == NULL)
+				{
 					printf("(nil)");
+				}
 				else
+				{
 					printf("%s", str);
+				}
 				break;
 			default:
 				break;
+
 		}
-		if (format[i + 1] && i != '\0')
+		if (format[i + 1])
 			printf(", ");
 		i++;
+
 	}
+
 	printf("\n");
 	va_end(pa_args);
 }
